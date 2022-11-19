7ba5165c46018499fa920112404f10a0e38b3cb8
Abdullahamdy <abdullahhamdy29@gmail.com>
2022-11-19 22:07:33 +0200

    Merge

--- classes/DB.php
+++ classes/DB.php
@@ -4,7 +4,7 @@ class DB
     private static $_instance = null;
     private $_pdo,
         $_query,
-        $_errors = false,
+        $_error = false,
         $_result,
         $_count = 0;
     private function __construct()
@@ -17,7 +17,6 @@ class DB
                 Config::get('mysql/username'),
                 Config::get('mysql/password')
             );
-            echo 'Connected';
         } catch (PDOException $e) {
             die($e->getMessage());
         }
@@ -29,5 +28,59 @@ class DB
             self::$_instance = new DB();
         }
         return self::$_instance;
+    }
+
+
+    public function query($sql, $params = array())
+    {
+        $this->_error = false;
+        if($this->_query = $this->_pdo->prepare($sql)){
+            if(count($params)){
+                $x = 1;
+                foreach($params as  $param){
+                    $this->_query->bindValue($x,$param);
+                    $x++;
+                }
+            }
+            if($this->_query->execute()){
+                $this->_result = $this->_query->fetchAll(PDO::FETCH_OBJ);
+                $this->_count = $this->_query->rowCount(); 
+            }else{
+                $this->_error = true;
+            }
+        }
+        return $this;
+    }
+
+
+    public function error(){
+        return $this->_error;
+
+    }
+
+    public function action($action , $table,$where=array()){
+        if(count($where) === 3){
+            $operators = array('=','<','>','>=','<=');
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+            if(in_array($operator,$operators)){
+                $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+                if(!$this->query($sql,array($value))->error()){
+                    return $this;
+                }
+            }
+        }
+            return false;
+    }
+
+    public function get($table,$where){
+        return $this->action('SELECT *',$table,$where);
+    }
+    public function delete($table,$where){
+        return $this->action('DELETE',$table,$where);
+    }
+    public function count(){
+        return $this->_count;
     }
 }
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 require_once('core/init.php');
-DB::getInstance();
+$user = DB::getInstance()->get('users',array('username','=','abdullah'));
+if(!$user->count()){
+echo 'NO User';
+}else{
+echo 'OK';
+}
